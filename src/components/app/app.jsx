e4eb5e15e7fe17b6bf998ad196ef165e3cfb9fa1
GTech1256 <pomah156@gmail.com>
2020-02-07 13:23:03 +0300

    Удалит комментарии Eslint

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,8 +3,8 @@ import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 import PropTypes from "prop-types";
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {errorsCount} = props;
+
   return (
     <WelcomeScreen errorsCount={errorsCount} />
   );
