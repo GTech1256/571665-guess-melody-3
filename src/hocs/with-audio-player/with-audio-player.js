5d7fe6df2e5576c8a1f4ece5b3cf4803708281b3
GTech1256 <pomah156@gmail.com>
2020-03-06 13:32:36 +0300

    Изменяет стрелочную функцию на метод

--- src/hocs/with-audio-player/with-audio-player.js
+++ src/hocs/with-audio-player/with-audio-player.js
@@ -1,5 +1,5 @@
 import React, {PureComponent} from "react";
-import AudioPlayer from "../../components/audio-player/audio-player";
+import AudioPlayer from "../../components/audio-player/audio-player.jsx";
 
 const withActivePlayer = (Component) => {
   class WithActivePlayer extends PureComponent {
@@ -9,24 +9,32 @@ const withActivePlayer = (Component) => {
       this.state = {
         activePlayerId: 0,
       };
+
+      this.renderPlayer = this.renderPlayer.bind(this);
+    }
+
+    renderPlayer(src, id) {
+      const {activePlayerId} = this.state;
+
+      return (
+        <AudioPlayer
+          src={src}
+          isPlaying={id === activePlayerId}
+          onPlayButtonClick={this.handlePlayButtonClick.bind(this, activePlayerId, id)}
+        />
+      );
+    }
+
+    handlePlayButtonClick(activePlayerId, id) {
+      this.setState({
+        activePlayerId: activePlayerId === id ? -1 : id
+      });
     }
 
     render() {
-      const {activePlayerId} = this.state;
-
       return <Component
         {...this.props}
-        renderPlayer={(src, id) => {
-          return (
-            <AudioPlayer
-              src={src}
-              isPlaying={id === activePlayerId}
-              onPlayButtonClick={() => this.setState({
-                activePlayerId: activePlayerId === id ? -1 : id
-              })}
-            />
-          );
-        }}
+        renderPlayer={this.renderPlayer}
       />;
     }
   }
